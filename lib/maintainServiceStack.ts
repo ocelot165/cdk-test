0d5a570a434c81cc934a798b7e9a1a1511443693
Revolver0celot <revocelot165@gmail.com>
2024-08-28 14:08:05 +0400

    Updated lambdas to use nodejsfunction

--- lib/maintainServiceStack.ts
+++ lib/maintainServiceStack.ts
@@ -4,21 +4,42 @@ import * as cdk from "aws-cdk-lib";
 import { DynamoDBStreamsToLambda } from "@aws-solutions-constructs/aws-dynamodbstreams-lambda";
 import { Construct } from "constructs";
 import path from "path";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 
 export class MaintainServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const { lambdaFunction: userFacingLambda, apiGateway } =
-      new ApiGatewayToLambda(this, "ApiGatewayToLambdaPattern", {
-        lambdaFunctionProps: {
-          runtime: cdk.aws_lambda.Runtime.NODEJS_20_X,
-          handler: "index.handler",
-          code: cdk.aws_lambda.Code.fromAsset(
-            path.join(__dirname, "..", "resources", "maintainServiceApi.ts")
-          ),
+    const userFacingLambda = new NodejsFunction(this, "UserFacingLambda", {
+      entry: path.join(
+        __dirname,
+        "..",
+        "resources",
+        "maintainServiceApi",
+        "index.ts"
+      ),
+      ...{
+        depsLockFilePath: path.join(
+          __dirname,
+          "..",
+          "resources",
+          "maintainServiceApi",
+          "package-lock.json"
+        ),
+        runtime: cdk.aws_lambda.Runtime.NODEJS_20_X,
+      },
+    });
+
+    const { apiGateway } = new ApiGatewayToLambda(
+      this,
+      "ApiGatewayToLambdaPattern",
+      {
+        existingLambdaObj: userFacingLambda,
+        apiGatewayProps: {
+          proxy: false,
         },
-      });
+      }
+    );
 
     const createPonderApi = apiGateway.root.addResource("createPonderService");
     createPonderApi.addMethod("POST");
@@ -36,17 +57,43 @@ export class MaintainServiceStack extends cdk.Stack {
       "LambdaToDynamoDBPattern",
       {
         existingLambdaObj: userFacingLambda,
+        dynamoTableProps: {
+          tableName: "serviceTable",
+          partitionKey: {
+            name: "id",
+            type: cdk.aws_dynamodb.AttributeType.STRING,
+          },
+          stream: cdk.aws_dynamodb.StreamViewType.NEW_IMAGE,
+        },
+      }
+    );
+
+    const dynamoTriggerLambda = new NodejsFunction(
+      this,
+      "DynamoTriggerLambda",
+      {
+        entry: path.join(
+          __dirname,
+          "..",
+          "resources",
+          "dynamoTriggerLambda",
+          "index.ts"
+        ),
+        ...{
+          depsLockFilePath: path.join(
+            __dirname,
+            "..",
+            "resources",
+            "dynamoTriggerLambda",
+            "package-lock.json"
+          ),
+          runtime: cdk.aws_lambda.Runtime.NODEJS_20_X,
+        },
       }
     );
 
     new DynamoDBStreamsToLambda(this, "DynamoDbToLambdaPattern", {
-      lambdaFunctionProps: {
-        code: cdk.aws_lambda.Code.fromAsset(
-          path.join(__dirname, "..", "resources", "createService.ts")
-        ),
-        runtime: cdk.aws_lambda.Runtime.NODEJS_20_X,
-        handler: "index.handler",
-      },
+      existingLambdaObj: dynamoTriggerLambda,
       existingTableInterface: dynamoTable,
     });
   }
