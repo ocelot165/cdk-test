3ad254033b1cacf0d5c15272bb5f7a2963f8dae2
Revolver0celot <revocelot165@gmail.com>
2024-08-28 14:07:36 +0400

    Added maintainservicestack to cdk compilation

--- bin/ponderStack.ts
+++ bin/ponderStack.ts
@@ -2,8 +2,18 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import InfraStack from "../lib/ponderStack";
+import { MaintainServiceStack } from "../lib/maintainServiceStack";
+import { execSync } from "child_process";
 
 const app = new cdk.App();
-new InfraStack(app, "PonderStack", {
+const stack = new InfraStack(app, "PonderStack", {
+  env: { region: "us-east-1" },
+});
+
+execSync(
+  `cp -f ./cdk.out/${stack.templateFile} ./resources/dynamoTriggerLambda`
+);
+
+new MaintainServiceStack(app, "MaintainServiceStack", {
   env: { region: "us-east-1" },
 });
