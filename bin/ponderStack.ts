745037e65a45fd7a41228c3d051aa2e06c6d25b6
Revolver0celot <revocelot165@gmail.com>
2024-09-01 12:44:26 +0400

    Added local config to the ponder stack(when context is not from lambda)

--- bin/ponderStack.ts
+++ bin/ponderStack.ts
@@ -4,10 +4,14 @@ import * as cdk from "aws-cdk-lib";
 import InfraStack from "../lib/ponderStack";
 import { MaintainServiceStack } from "../lib/maintainServiceStack";
 import { execSync } from "child_process";
+import { getConfig } from "../lib/config";
+
+const config = getConfig();
 
 const app = new cdk.App();
 const stack = new InfraStack(app, "PonderStack", {
   env: { region: "us-east-1" },
+  config: config.PONDER_STACK_CONTEXT === "USER" ? config : undefined,
 });
 
 execSync(
---
+++ lib/config.ts
@@ -0,0 +1,25 @@
+import * as dotenv from "dotenv";
+import path = require("path");
+
+// 1. Configure dotenv to read from our `.env` file
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+// 2. Define a TS Type to type the returned envs from our function below.
+export type ConfigProps = {
+  GITHUB_URL?: string;
+  USER_ID?: string;
+  GITHUB_TOKEN?: string;
+  CHAIN_ID?: string;
+  RPC_URL?: string;
+  PONDER_STACK_CONTEXT?: string;
+};
+
+// 3. Define a function to retrieve our env variables
+export const getConfig = (): ConfigProps => ({
+  GITHUB_URL: process.env.GITHUB_URL,
+  USER_ID: process.env.USER_ID,
+  GITHUB_TOKEN: process.env.GITHUB_TOKEN,
+  CHAIN_ID: process.env.CHAIN_ID,
+  RPC_URL: process.env.RPC_URL,
+  PONDER_STACK_CONTEXT: process.env.PONDER_STACK_CONTEXT,
+});
